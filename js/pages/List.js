120ee069a2670a6c8f2ad78e9edac13416bd0da6
SkoeGMD <125868022+Alix2580@users.noreply.github.com>
2025-04-08 22:02:45 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -16,7 +16,7 @@ const roleIconMap = {
 
 export default {
     components: { Spinner, LevelAuthors },
-    template: `
+    template: 
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
@@ -30,16 +30,7 @@ export default {
                         </td>
                         <td class="level" :class="{ 'active': selected == i, 'error': err !== null }">
                             <button @click="selected = i">
-                                <span
-                                  class="type-label-lg"
-                                  :class="{
-                                    gold: i === 0,
-                                    silver: i === 1,
-                                    bronze: i === 2
-                                  }"
-                                >
-                                  {{ level?.name || `Error (${err}.json)` }}
-                                </span>
+                                <span class="type-label-lg">{{ level?.name || \Error (\${err}.json)\ }}</span>
                             </button>
                         </td>
                     </tr>
@@ -95,7 +86,7 @@ export default {
                                 <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
                             </td>
                             <td class="mobile">
-                                <img v-if="record.mobile" :src="`/assets/phone-landscape${store.dark ? '-dark' : ''}.svg`" alt="Mobile">
+                                <img v-if="record.mobile" :src="\/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\" alt="Mobile">
                             </td>
                         </tr>
                     </table>
@@ -116,7 +107,7 @@ export default {
                         <h3>List Editors</h3>
                         <ol class="editors">
                             <li v-for="editor in editors">
-                                <img :src="`/assets/${roleIconMap[editor.role]}${store.dark ? '-dark' : ''}.svg`" :alt="editor.role">
+                                <img :src="\/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\" :alt="editor.role">
                                 <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
                                 <p v-else>{{ editor.name }}</p>
                             </li>
@@ -147,7 +138,7 @@ export default {
                 </div>
             </div>
         </main>
-    `,
+    ,
     data: () => ({
         list: [],
         editors: [],
@@ -189,7 +180,7 @@ export default {
                 ...this.list
                     .filter(([err, _, __]) => err)
                     .map(([err, _, __]) => {
-                        return `Failed to load level. (${err}.json)`;
+                        return Failed to load level. (${err}.json);
                     }),
             );
             if (!this.editors) {

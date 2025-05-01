89fd5d83aadf6f4ba293582e32c5f54122a5ec1a
SkoeGMD <125868022+Alix2580@users.noreply.github.com>
2025-05-01 16:48:55 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,11 +2,9 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Submit from './pages/Submit.js';
-import Legacy from './pages/Legacy.js'; // ✅ make sure this path and name are correct
 
 export default [
     { path: '/', component: List },
-    { path: '/legacy', component: Legacy }, // ✅ this defines your new tab
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/submit', component: Submit },

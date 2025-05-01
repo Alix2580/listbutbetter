f728dc6be09453f3e02a42c061f7fc83a735fd47
SkoeGMD <125868022+Alix2580@users.noreply.github.com>
2025-05-01 16:44:55 +0200

    Delete js/Legacy.js

--- js/Legacy.js
+++
@@ -1,8 +0,0 @@
-import { createListView } from './SharedListView.js';
-import { fetchList } from './content.js';
-
-export default createListView({
-    fetchList,
-    hidePoints: true,
-    filter: ([, rank]) => rank === null || rank > 100,
-});
